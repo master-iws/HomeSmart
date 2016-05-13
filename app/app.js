7232c8050b5de0952bd788417f81b10bd557e8df
René Lottes <rene.lottes@hof-university.de>
2016-05-13 12:33:36 +0200

    Fixed whitespaces

--- app/app.js
+++ app/app.js
@@ -4,22 +4,22 @@ var app = angular.module("HomeSmart", ["ng", "ui.router", "ui.router.title"]);
 
 //Debugging events
 app.config(['$provide', function ($provide) {
-  $provide.decorator('$rootScope', function ($delegate) {
-    var _emit = $delegate.$emit;
-    var _broadcast = $delegate.$broadcast;
+	$provide.decorator('$rootScope', function ($delegate) {
+		var _emit = $delegate.$emit;
+		var _broadcast = $delegate.$broadcast;
 
-    $delegate.$emit = function () {
-      console.log("[$emit] " + arguments[0], arguments);
-      return _emit.apply(this, arguments);
-    };
+		$delegate.$emit = function () {
+			console.log("[$emit] " + arguments[0], arguments);
+			return _emit.apply(this, arguments);
+		};
 
-    $delegate.$broadcast = function () {
-      console.log("[$broadcast] " + arguments[0], arguments);
-      return _broadcast.apply(this, arguments);
-    };
+		$delegate.$broadcast = function () {
+			console.log("[$broadcast] " + arguments[0], arguments);
+			return _broadcast.apply(this, arguments);
+		};
 
-    return $delegate;
-  });
+		return $delegate;
+	});
 }]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
@@ -30,7 +30,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 			url: "",
 			resolve: {
 				title: function() { return 'index'; }
-            },
+			},
 			//controller: 'LandingController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.index.htm"},
@@ -41,7 +41,7 @@ app.config(function($stateProvider, $urlRouterProvider) {
 			url: "/houses",
 			resolve: {
 				$title: function() { return 'houses'; }
-            },
+			},
 			//controller: 'HousesController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.houses.htm"},
@@ -51,8 +51,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		}).state("floors", {
 			url: "/floors",
 			resolve: {
-                $title: function() { return 'Floors'; }
-            },
+				$title: function() { return 'Floors'; }
+			},
 			//controller: 'FloorsController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.houses.htm"},
@@ -62,8 +62,8 @@ app.config(function($stateProvider, $urlRouterProvider) {
 		}).state("floors.detail", {
 			url: "/:floorId",
 			resolve: {
-                $title: function() { return 'Floors..'; }
-            },
+				$title: function() { return 'Floors..'; }
+			},
 			//controller: 'FloorsController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.houses.htm"},
