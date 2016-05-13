a3e6ac6bef4cd289318f86facf476bf5c2e463d1
René Lottes <rene.lottes@hof-university.de>
2016-05-13 12:30:36 +0200

    WIP ui.router.title

--- app/app.js
+++ app/app.js
@@ -1,6 +1,26 @@
 'use strict';
 
-var app = angular.module('HomeSmart', ['ui.router']);
+var app = angular.module("HomeSmart", ["ng", "ui.router", "ui.router.title"]);
+
+//Debugging events
+app.config(['$provide', function ($provide) {
+  $provide.decorator('$rootScope', function ($delegate) {
+    var _emit = $delegate.$emit;
+    var _broadcast = $delegate.$broadcast;
+
+    $delegate.$emit = function () {
+      console.log("[$emit] " + arguments[0], arguments);
+      return _emit.apply(this, arguments);
+    };
+
+    $delegate.$broadcast = function () {
+      console.log("[$broadcast] " + arguments[0], arguments);
+      return _broadcast.apply(this, arguments);
+    };
+
+    return $delegate;
+  });
+}]);
 
 app.config(function($stateProvider, $urlRouterProvider) {
 	
@@ -8,6 +28,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
 	
 	$stateProvider.state("index", {
 			url: "",
+			resolve: {
+				title: function() { return 'index'; }
+            },
 			//controller: 'LandingController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.index.htm"},
@@ -16,6 +39,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
 			}
 		}).state("houses", {
 			url: "/houses",
+			resolve: {
+				$title: function() { return 'houses'; }
+            },
 			//controller: 'HousesController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.houses.htm"},
@@ -24,6 +50,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
 			}
 		}).state("floors", {
 			url: "/floors",
+			resolve: {
+                $title: function() { return 'Floors'; }
+            },
 			//controller: 'FloorsController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.houses.htm"},
@@ -32,6 +61,9 @@ app.config(function($stateProvider, $urlRouterProvider) {
 			}
 		}).state("floors.detail", {
 			url: "/:floorId",
+			resolve: {
+                $title: function() { return 'Floors..'; }
+            },
 			//controller: 'FloorsController',
 			views: {
 				"Nav1": { templateUrl: "app/views/nav/nav1.houses.htm"},
--- app/controller/Ctr_Main.js
+++ app/controller/Ctr_Main.js
@@ -1,5 +1,18 @@
 'use strict';
 
-app.controller('MainController', function($scope) {
-       $scope.greet = "It work's!";
-});
+app.controller('MainController',
+	["$scope", "$rootScope", "$state",
+	function($scope, $rootScope, $state) {
+		$scope.greet = "It work's!";
+		$scope.TITLE = "It work's!";
+		
+		$rootScope.$on("$stateChangeStart", function (event, toState, toParams, fromState) {
+			$scope.greet = "It work'sddd!";
+			console.log(",,,");
+		});
+		
+		setTimeout(function(){
+			$state.go("floors");
+			console.log("sdf");
+		},3000);
+}]);
