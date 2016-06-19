949baee907e967b7831b815a03207f02d4fcebeb
René Lottes <rene.lottes@hof-university.de>
2016-06-19 09:18:41 +0200

    Added Room

---
+++ app/models/Mod_Room.js
@@ -0,0 +1,55 @@
+/**
+ * @author René Lottes
+ */
+
+'use strict';
+
+function Mod_Room() {
+    
+    var _floor,
+	_components = [];
+
+    
+    /*
+     * getter
+     */
+    
+    this.getFloor = function() {
+	return _floor;
+    };
+    
+    this.getCompnents = function() {
+	return _components;
+    };
+    
+    /*
+     * setter TODO
+     */
+    
+    this.setFloor = function(floor) {
+	if(!(floor instanceof Mod_Floor)) {
+	    throw new TypeError();
+	}
+	_floor = floor;
+    };
+    
+    this.addComponent = function(component) {
+	if(!(component instanceof Mod_Component)) {
+	    throw new TypeError();
+	}
+	_components.push(component);
+    };
+    
+    //TODO: remove dosen't work!
+    this.removeComponent = function(component) {
+	if(!(component instanceof Mod_Component || this.checkNum(component))) {
+	    throw new TypeError();
+	}
+	_components = _components.splice(_components.indexOf(component),1);
+    };
+    
+    
+    Mod_Abstract_Entity.call(this);
+
+}
+
