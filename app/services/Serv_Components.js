0ae77a36d52188cf360a9177cf6312dd66629cb5
René Lottes <rene.lottes@hof-university.de>
2016-06-20 15:58:41 +0200

    Added service for cats and comp

---
+++ app/services/Serv_Components.js
@@ -0,0 +1,163 @@
+/**
+ * @author René Lottes
+ */
+'use strict';
+
+app.service("ComponentService", ["Mod_Category","Mod_Component",
+    function(Mod_Category,Mod_Component) {
+    
+    var id = 1000;
+    var base_id = 1;
+    
+    var cats = [];
+    
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Garten");
+	fillComponents(cat,["Rasenroboter","Zisterne","Sprenkelanlage"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Haushaltsgeräte");
+	fillComponents(cat,["Badewanne","Kaffeemaschine","Herd","Spülmaschine","Kühlschrank","Waschmaschine"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Wellness");
+	fillComponents(cat,["Sauna","Pool"]);
+	cats.push(cat);
+	increaseId();
+
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Zentral");
+	fillComponents(cat,["Photovoltaikanlage"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Heimkino");
+	fillComponents(cat,["Beamer","TV","Leinwand"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Verbraucher");
+	fillComponents(cat,["Energieverbraucher","Wasserverbraucher"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Klima");
+	fillComponents(cat,["Temperaturmesser","Niederschlagsmesser","Luftdruckmesser","Windgeschwindigkeitsmesser"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Beleuchtung");
+	fillComponents(cat,["Taster","Dimmer","Effektlicht"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Fenster, Türen & Tore");
+	fillComponents(cat,["Garagentor","Tür","Fenster"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Kamera");
+	fillComponents(cat,["Innenkamera","Außenkamera"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Bewegunsmelder");
+	fillComponents(cat,["Bewegunsmelder außen","Bewegunsmelder innen"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Multi Room Audio");
+	fillComponents(cat,["Multi Room Audio"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Heizen");
+	fillComponents(cat,["Innenthermostat","Heizkörperthermostat","Zentralheiung"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Alarm");
+	fillComponents(cat,["Alarmanlage"]);
+	cats.push(cat);
+	increaseId();
+	
+    var cat = new Mod_Category();
+	cat.setId(getNextId());
+	cat.setName("Melder");
+	fillComponents(cat,["Rauchmelder"]);
+	cats.push(cat);
+	increaseId();
+
+
+    function getCategorys() {
+	return cats;
+    }
+    
+    function getCategoryById(id) {
+	
+    }
+    
+    function getComponentsByCategory(id) {
+	
+    }
+    
+    function getComponentById(id) {
+	
+    }
+    
+    function fillComponents(cat,comp_arr) {
+	for(var i in comp_arr) {
+	    var comp = new Mod_Component();
+	    comp.setId(getNextId());
+	    comp.setName(comp_arr[i]);
+	    //TODO: description / type / settings
+	    comp.setCategory(cat);
+	    cat.addComponent(comp);
+	}
+    }
+    
+    function getNextId() {
+	return id++;
+    }
+    function increaseId() {
+	//1xxx per cat
+	//xYYY per comp
+	id = base_id*1000;
+	base_id++;
+    }
+    
+    return {
+	getCategorys:getCategorys,
+	getCategoryById:getCategoryById,
+	getComponentsByCategory:getComponentsByCategory,
+	getComponentById:getComponentById
+    };
+}]);
