b11ece18032d345c89e5002da6a04aa2bc68f39f
Julia <julia.thueroff@hof-university.de>
2016-06-20 11:39:26 +0200

    main merged

---
+++ app/controller/Ctr_Main.js
@@ -0,0 +1,17 @@
+//'use strict';
+
+app.controller('MainController',
+	["$scope", "$rootScope", "$state","MainService",
+	function($scope, $rootScope, $state, mainService) {
+		
+		$rootScope.adminPin = "test12";
+		$rootScope.houses = [];
+		$rootScope.houses.push({"name": "test", "city": "95119 Naila"});
+		$rootScope.houses.push({"name": "test1", "city": "95119 Naila"});
+		$rootScope.houseIndex = $rootScope.houses.length-1;
+		$rootScope.loggedIn = true;
+		
+		var test = mainService.getHouses();
+		console.log(JSON.stringify(test));
+		mainService.saveHouses(test);
+}]);
