d1304648d5bdcdb4cda0e468eaab140ad38dd002
Matthias Jakob <matthias.jakob@hof-university.de>
2016-06-30 16:09:42 +0200

    Neue Komponente hinzugefügt

---
+++ app/controller/component/Comp_AirPressure.js
@@ -0,0 +1,8 @@
+function AirPressureController($scope, $element, $attrs) {
+	
+}
+
+app.component('compAirPressure', {
+	templateUrl: 'app/views/component/airPressure.htm',
+	controller: AirPressureController
+});
