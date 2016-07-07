106d6fceda1fc45be3d35749586e2623df42caf8
Matthias Jakob <matthias.jakob@hof-university.de>
2016-07-07 20:43:59 +0200

    Bugfix und Zentral erweitert

---
+++ app/directive/component/Comp_Central.js
@@ -0,0 +1,23 @@
+app.directive('compCentral', function() {
+	
+	return {
+		templateUrl: 'app/views/component/central.htm',
+		controller: ['$scope', '$rootScope', 'vibrator', function($scope, $rootScope, vibrator) {
+
+			$scope.lightOff = function() {
+				vibrator.vibrate(1000);
+				$rootScope.houses[$rootScope.houseIndex].setLight(0);
+			};
+
+			$scope.consumerOff = function() {
+				vibrator.vibrate(1000);
+				$rootScope.houses[$rootScope.houseIndex].setConsumer(0);
+			};
+
+			$scope.setShadowing = function($value) {
+				vibrator.vibrate(1000);
+				$rootScope.houses[$rootScope.houseIndex].setShadowing($value);
+			};
+		}]
+	};
+});
