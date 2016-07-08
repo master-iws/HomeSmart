1bac4d2140251c03dc123d0c9a2c0fc2c9a7c807
Matthias Jakob <matthias.jakob@hof-university.de>
2016-07-08 11:49:15 +0200

    Kachel angepasst

--- app/controller/Ctr_Rooms.js
+++ app/controller/Ctr_Rooms.js
@@ -1,10 +1,9 @@
 'use strict';
 
-app.controller('RoomsController',["$scope", "$rootScope", "$state","$uibModal","vibrator",	
+app.controller('RoomsController',["$scope", "$rootScope", "$state","$uibModal","vibrator",
                                                     function($scope, $rootScope, $state,$uibModal,vibrator) {
 	
 	$scope.rooms = $rootScope.houses[$rootScope.houseIndex].getRooms();
-	
 
     $scope.addRoom = function() {
     	$state.go("houseconfiguration.rooms.addRoom");
@@ -15,32 +14,25 @@ app.controller('RoomsController',["$scope", "$rootScope", "$state","$uibModal","
 	};
     
     $scope.editRoom = function($roomIdx) {
-    	console.log("edit room");
     	$state.go("houseconfiguration.rooms.editRoom",{'roomId':$roomIdx});
     };
     
     $scope.lightOff = function($roomIdx) {
     	vibrator.vibrate(1000);
     	$rootScope.houses[$rootScope.houseIndex].getRooms()[$roomIdx].setLight(0);
-    };	
+    };
     
     $scope.consumerOff = function($roomIdx) {
     	vibrator.vibrate(1000);
     	$rootScope.houses[$rootScope.houseIndex].getRooms()[$roomIdx].setConsumer(0);
-    };	
+    };
 	
     $scope.setShadowing = function($roomIdx, $value) {
     	vibrator.vibrate(1000);
     	$rootScope.houses[$rootScope.houseIndex].getRooms()[$roomIdx].setShadowing($value);
     };
     
-   $scope.addRoom = function() {
-    	
-    	$state.go("houseconfiguration.rooms.addRoom");
-    };
-    
     $scope.configureRoom = function($roomId) {
-    	
     	$state.go("houseconfiguration.rooms.editRoom",{roomId: $roomId});
     };
 }]);
